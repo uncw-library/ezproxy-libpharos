b9d25bb8bec6d3463d1de1873c47a4255fefd87d
ElliotSimpson <31009469+ElliotSimpson@users.noreply.github.com>
2017-10-24 16:08:08 -0400

    production ready

--- app.js
+++ app.js
@@ -82,9 +82,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/guest-access-job', express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/guest-access-job', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
